57d59915bd2ab0bc4a3c2eb7e095b160270ae675
SethLawson07 <latesethlawsonhetchely@gmail.com>
2022-12-28 11:46:24 +0000

    jwt implementation

--- routes/user.js
+++ routes/user.js
@@ -8,28 +8,7 @@ const {
     deleteUser,
 } = require("../controllers/user");
 const router = express.Router();
-const dotenv = require("dotenv");
-const jwt = require('jsonwebtoken');
 
-dotenv.config();
-
-
-const verifyUserToken = (req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(401).send("Unauthorized request");
-  }
-  const token = req.headers["authorization"].split(" ")[1];
-  if (!token) {
-    return res.status(401).send("Access denied. No token provided.");
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(400).send("Invalid token.");
-  }
-};
 
 router.route("/users").post(addUser);
 router.route("/users").get(getUsers);
